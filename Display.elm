d7caf6a6dbb69b673cd37f408bf32b013ecfd678
asecular <asecular@uchicago.edu>
2015-02-23 11:48:01 -0600

    merging title display code into Display.elm

--- Display.elm
+++ Display.elm
@@ -22,27 +22,48 @@ import String
 import Keyboard as Key
 
 -- View
-testerFun : Box -> Int -> El.Element
-testerFun b bSide =
+
+title w h =
+    El.container w (h + 20) El.middle
+          <| El.flow El.down
+                 [El.image w h "/pacman-logo.jpg", El.spacer w 20]
+
+renderPacman : Pacman -> Int -> El.Element
+renderPacman p bSide =
+    let
+        pman_form =
+            case p.dir of
+              Left -> Mod.pacman Mod.Left ((toFloat bSide) / 2)
+              Right -> Mod.pacman Mod.Right ((toFloat bSide) / 2)
+              Up -> Mod.pacman Mod.Up ((toFloat bSide) / 2)
+              Down -> Mod.pacman Mod.Down ((toFloat bSide) / 2)
+    in
+      Clg.collage bSide bSide [pman_form]
+
+displayBox : Box -> Int -> El.Element
+displayBox b bSide =
     case b of
       Empty -> Clg.collage bSide bSide [Mod.emptySpace (toFloat bSide)]
       Pellet -> Clg.collage bSide bSide [Mod.pellet ((toFloat bSide) / 6)]
       Pill -> Clg.collage bSide bSide [Mod.pill ((toFloat bSide) / 3)]
       Wall -> Clg.collage bSide bSide [Mod.wall (toFloat bSide)]
 
-
 view : (Int, Int) -> State -> El.Element
 view (w, h) st =
     let
-        bSide = h // 36
-        rowBuilder bxs = El.flow El.left (List.map (\b -> testerFun b bSide) bxs)
-        colBuilder rws = El.flow El.down rws
-        pac_pos = Utl.itow (bSide * numCols) (bSide * numRows) st.pacman.pos
+        bSide = (h - 45) // 36
+        titleHeight = 30
+        titleWidth = bSide * 23
+        ttl = title titleWidth titleHeight
+        rowBuilder bxs = El.flow El.left (List.map (\b -> displayBox b bSide) bxs)
+        colBuilder rws = El.flow El.down ([ttl] ++ rws)
+        pac_pos = Utl.itow (bSide * numCols) (titleHeight + 20 + (bSide * numRows)) st.pacman.pos
         pac_dir = case st.pacman.dir of
                     Left -> Mod.Left
                     Right -> Mod.Right
                     Up -> Mod.Up
                     Down -> Mod.Down
+
     in
       El.color black
             <| Clg.collage w h
@@ -50,7 +71,7 @@ view (w, h) st =
                  , Clg.move pac_pos <| Mod.pacman pac_dir <| toFloat <| bSide // 2
                  ]
 
--- Controller
+--Controller
 
 type Action = KeyAction Key.KeyCode | TimeAction
 
