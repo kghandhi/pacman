068df04496225fa23daac9719f82d90904be08a6
kghandhi <kghandhi@uchicago.edu>
2015-03-08 15:28:06 -0500

    added the appropriate files

---
+++ Interactions.elm
@@ -0,0 +1,42 @@
+module Interactions where
+
+import Pacman (..)
+import List
+
+pif (x, y) = (truncate x, truncate y)
+
+-- Produces the ghost pacman overlaps with
+overlap: State -> (List Ghost, List Ghost)
+overlap st =
+    let
+        pacPos = pif st.pacman.pos
+        mapper g = (pif g.pos == pacPos) && (g.self /= Dead)
+        parter g = g.self == Normal
+    in
+      List.partition parter
+              <| List.filter mapper [st.blinky, st.pinky, st.inky, st.clyde]
+
+
+makeEyes g scared =
+    if List.member g scared then {g | self <- Dead
+                                 , mode <- House}
+    else g
+
+interact : State -> State
+interact st =
+    let
+        (scary, scared) = overlap st
+        numScared = List.length scared
+        killPoints = List.sum <| List.take numScared st.ghostPoints
+        lifeLoss = not <| List.isEmpty scary
+    in
+      case (scary, scared) of
+        ([], []) -> st
+        _ -> {st | points <- st.points + killPoints
+             , extraLives <- if lifeLoss then st.extraLives-1 else st.extraLives
+             , gameState <- Loading
+             , blinky <- makeEyes st.blinky scared
+             , pinky <- makeEyes st.pinky scared
+             , inky <- makeEyes st.inky scared
+             , clyde <- makeEyes st.clyde scared
+             , ghostPoints <- List.drop numScared st.ghostPoints}
