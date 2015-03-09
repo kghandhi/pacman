f841f1b1aae2b10ce13163191771da8c56e165bc
asecular <asecular@uchicago.edu>
2015-03-09 00:44:32 -0500

    got sound to work sort of and adding some files

---
+++ Audio.elm
@@ -0,0 +1,65 @@
+module Audio where
+
+{- A note from Abe and Kira: we did not write this module, 
+   it was written by Joseph Collard and can be found here:
+   https://github.com/jcollard/elm-audio -}
+
+{-| The Audio provides an interface for playing audio -}
+
+import Native.Audio
+import Signal
+import Keyboard
+import Set
+import Time (Time)
+
+{-| An Action controls how audio is heard. -}
+type Action = Play | Pause | Seek Time | SeekAndPause Time | NoChange
+
+{-| A Properties record contains information related to audio -}
+type alias Properties = { duration : Time, currentTime : Time, ended : Bool }
+
+{-| A Triggers record describes on what events you would like to receive
+    a Properties record -}
+type alias Triggers = { timeupdate : Bool, ended : Bool }
+
+{-| The defaultTriggers is a record for ease of use. All Triggers are
+    set to False. -}
+defaultTriggers : Triggers
+defaultTriggers = { timeupdate = False, ended = False }
+
+{-| An Event is used to describe why the Signal returned by the
+    audio function was fired.
+ -}
+type Event = TimeUpdate
+           | Ended
+           | Created
+
+{-| A Builder Record is used to desribe how audio should be treated.
+    src - The Path to an audio file
+    triggers - A Triggers Record describing on which events we want to receive Properties
+    propertiesHandler - A Function that is called each time Properties are calculated.
+    actions - A Signal of incomming actions that manipulate the audio -}
+type alias Builder = { src : String,
+                       triggers : Triggers,
+                       propertiesHandler : (Properties -> Maybe Action),
+                       actions : Signal Action }
+
+{-| Given a Builder, creates an Signal that fires on the specified events
+    and produces the properties during those events.
+-}
+audio : Builder -> Signal (Event, Properties)
+audio audioBuilder =
+    let handleEvent =
+            (\sound action ->
+                     case action of
+                       Play -> Native.Audio.play sound
+                       Pause -> Native.Audio.pause sound
+                       Seek t -> Native.Audio.seek sound t
+                       --SeekAndPause t -> Native.Audio.pause <| Native.Audio.seek sound t
+                       NoChange -> ())
+    in Native.Audio.audio 
+          handleEvent
+          audioBuilder.src
+          audioBuilder.triggers
+          audioBuilder.propertiesHandler
+          audioBuilder.actions
---
+++ Native/Audio.js
@@ -0,0 +1,86 @@
+
+Elm.Native.Audio = {};
+Elm.Native.Audio.make = function(elm) {
+    elm.Native = elm.Native || {};
+    elm.Native.Audio = elm.Native.Audio || {};
+    if (elm.Native.Audio.values) return elm.Native.Audio.values;
+    
+    // Imports
+    var Signal = Elm.Native.Signal.make(elm);
+    var Maybe = Elm.Maybe.make(elm);
+
+    var TimeUpdate = {ctor : "TimeUpdate"};
+    var Ended = {ctor : "Ended"};
+    var Created = {ctor : "Created"};
+
+    // Helper Functions... Do these exist already?
+    function Tuple2(fst, snd){
+        return {ctor: "_Tuple2", _0 : fst, _1 : snd};
+    }
+
+    function Properties(duration, currentTime, ended){
+        return { _ : {}, duration : duration, currentTime : currentTime, ended : ended};
+    }
+
+
+    // Creates a Signal (Event, Properties)
+    function audio(handler, path, alerts, propHandler, actions) {
+
+        var sound = new Audio(path);
+        var clock = new Object();
+        var event = Signal.constant(Tuple2(Created, Properties(0,0,0)));
+
+        var handle = handler({ "sound": sound, "clock": clock });
+
+        function fireProp(eventCons){
+            var props = Properties(sound.duration, sound.currentTime, sound.ended);
+            elm.notify(event.id, Tuple2(eventCons, props));
+            var action = propHandler(props);
+            if(action.ctor == "Just")
+                handle(action._0)
+        }
+
+        function addAudioListener(eventString, eventCons){
+            sound.addEventListener(eventString, function () { fireProp(eventCons); });
+        }
+
+        if(alerts.timeupdate)
+        {
+            var timer;
+            clock.start = function() { timer = setInterval(function(){ fireProp(TimeUpdate); }, 10); };
+            clock.stop = function() { clearInterval(timer); };
+        } else {
+            clock.start = function() {};
+            clock.stop = function() {};
+        }
+
+        if(alerts.ended)
+            addAudioListener('ended', Ended);
+
+        Signal.map(handle)(actions);
+        return event;
+    }
+
+    function play(o){
+        o.sound.play();
+        o.clock.start();
+    }
+
+    function pause(o){
+        o.clock.stop();
+        o.sound.pause()
+    }
+
+    function seek(o, time){
+        o.sound.currentTime = time;
+    }
+
+    return elm.Native.Audio.values = {
+        audio : F5(audio),
+        play : play,
+        pause : pause,
+        seek : F2(seek)
+    };
+
+};
+
