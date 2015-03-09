62fd109cbec9fa65bc25f28adf62f8cd9ecb6379
asecular <asecular@uchicago.edu>
2015-03-08 19:24:28 -0500

    added start screen (options not implemented)

--- Display.elm
+++ Display.elm
@@ -21,6 +21,7 @@ import Mouse
 import Text as Txt
 import Graphics.Element as El
 import Graphics.Collage as Clg
+import Graphics.Input   as Inp
 import String
 import Keyboard as Key
 
@@ -37,21 +38,21 @@ renderPacman p bSide =
     let
         pman_form =
             case p.dir of
-              Left -> Mod.pacman Mod.Left ((toFloat bSide) / 2)
+              Left  -> Mod.pacman Mod.Left ((toFloat bSide) / 2)
               Right -> Mod.pacman Mod.Right ((toFloat bSide) / 2)
-              Up -> Mod.pacman Mod.Up ((toFloat bSide) / 2)
-              Down -> Mod.pacman Mod.Down ((toFloat bSide) / 2)
+              Up    -> Mod.pacman Mod.Up ((toFloat bSide) / 2)
+              Down  -> Mod.pacman Mod.Down ((toFloat bSide) / 2)
     in
       Clg.collage bSide bSide [pman_form]
 
 displayBox : Box -> Int -> El.Element
 displayBox b bSide =
     case b of
-      Empty -> Clg.collage bSide bSide [Mod.emptySpace (toFloat bSide)]
+      Empty  -> Clg.collage bSide bSide [Mod.emptySpace (toFloat bSide)]
       Pellet -> Clg.collage bSide bSide [Mod.pellet ((toFloat bSide) / 6)]
-      Pill -> Clg.collage bSide bSide [Mod.pill ((toFloat bSide) / 3)]
-      Wall -> Clg.collage bSide bSide [Mod.wall (toFloat bSide)]
-      Gate -> Clg.collage bSide bSide [Mod.gate (toFloat bSide)]
+      Pill   -> Clg.collage bSide bSide [Mod.pill ((toFloat bSide) / 3)]
+      Wall   -> Clg.collage bSide bSide [Mod.wall (toFloat bSide)]
+      Gate   -> Clg.collage bSide bSide [Mod.gate (toFloat bSide)]
 
 displayLives : Int -> Int -> El.Element
 displayLives lives sz =
@@ -64,11 +65,11 @@ magicNumber = 45
 
 scoreStyle : Txt.Style
 scoreStyle = {typeface = font
-             , height = Just magicNumber
-             , color = white
-             , bold = False
-             , italic = False
-             , line = Nothing
+             , height  = Just magicNumber
+             , color   = white
+             , bold    = False
+             , italic  = False
+             , line    = Nothing
              }
 
 renderGhost g bSide =
@@ -78,15 +79,62 @@ renderGhost g bSide =
     in
       case g.self of
         Scared -> Mod.ghost "scared" w h
-        Dead -> Mod.ghost "dead" w h
+        Dead   -> Mod.ghost "dead" w h
         Normal -> Mod.ghost g.name w h
+
+startMenu : Int -> Int -> State -> Clg.Form
+startMenu w h st =
+  let
+    (fw, fh) = (toFloat w, toFloat h)
+    lin_stl  = Clg.solid darkYellow
+    lin_stl' = {lin_stl | width <- 7}
+    el_wt = fw * 0.8
+    el_ht = fh / 5
+    button_general c str = 
+      Clg.collage (round el_wt) (round el_ht)
+        [ Clg.filled black <| Clg.rect (0.95 * el_wt)  (0.96 * el_ht)
+        , Clg.filled c     <| Clg.rect (0.92  * el_wt)  (0.9  * el_ht)
+        , Clg.toForm       <| Txt.centered
+                           <| Txt.color black
+                           <| Txt.height (fh / 8)
+                           <| Txt.typeface font
+                           <| Txt.fromString str
+        ]
+    button_up    str = button_general yellow      str
+    button_hover str = button_general lightYellow str
+    button_down  str = button_general darkYellow  str
+    myButton msg str =
+      Clg.toForm
+        <| Inp.customButton
+            (Signal.send actionChannel (ButtonAction msg))
+            (button_up    str)
+            (button_hover str)
+            (button_down  str)
+  in
+    Clg.toForm
+      <| Clg.collage w h
+           [ Clg.filled   darkBlue      <| Clg.rect fw fh
+           , Clg.outlined lin_stl'      <| Clg.rect fw fh
+           , Clg.moveY (fh / 4)    
+              <| Clg.group 
+                   [ Clg.filled yellow  <| Clg.oval (1.05 * el_wt) (1.05 * el_ht)
+                   , Clg.filled black   <| Clg.oval el_wt el_ht
+                   , Clg.toForm         <| Txt.centered 
+                                        <| Txt.color yellow
+                                        <| Txt.height  (fh / 8)
+                                        <| Txt.typeface font
+                                        <| Txt.fromString "Pac-Man"
+                   ]
+           , Clg.moveY (-fh / 15)       <| myButton Go "Start"
+           , Clg.moveY (-4.3 * fh / 15) <| myButton Go "Options"
+           ]
 
 view : (Int, Int) -> State -> El.Element
 view (w, h) st =
     let
-        bSide = (h - magicNumber) // 36
+        bSide       = (h - magicNumber) // 36
         titleHeight = 30
-        titleWidth = bSide * 23
+        titleWidth  = bSide * 23
 
         ttl = title titleWidth titleHeight
 
@@ -99,47 +147,68 @@ view (w, h) st =
         gState_pos = Utl.itow (bSide * numCols) (titleHeight + 20 + (bSide * numRows)) (13.5, 17)
         gState = case st.gameState of
                    Loading -> El.fittedImage (6 * bSide) bSide "/loading.png"
-                   Over -> El.fittedImage (8 * bSide) bSide "/over.png"
-                   Active -> El.empty
+                   Over    -> El.fittedImage (8 * bSide) bSide "/over.png"
+                   _       -> El.empty
 
         rowBuilder bxs = El.flow El.left (List.map (\b -> displayBox b bSide) bxs)
         colBuilder rws = El.flow El.down ([scoreLives] ++ rws)
 
-        pinky_pos = Utl.itow (bSide * numCols) (titleHeight + 20 + (bSide * numRows)) st.pinky.pos
-        inky_pos = Utl.itow (bSide * numCols) (titleHeight + 20 + (bSide * numRows)) st.inky.pos
-        blinky_pos = Utl.itow (bSide * numCols) (titleHeight + 20 + (bSide * numRows)) st.blinky.pos
-        clyde_pos = Utl.itow (bSide * numCols) (titleHeight + 20 + (bSide * numRows)) st.clyde.pos
-        pac_pos = Utl.itow (bSide * numCols) (titleHeight + 20 + (bSide * numRows)) st.pacman.pos
-        pac_dir = case st.pacman.dir of
-                    Left -> Mod.Left
-                    Right -> Mod.Right
-                    Up -> Mod.Up
-                    Down -> Mod.Down
+        wFromBxs = bSide * numCols
+        hFromBxs = (titleHeight + 20 + (bSide * numRows))
+
+        pinky_pos  = Utl.itow wFromBxs hFromBxs st.pinky.pos
+        inky_pos   = Utl.itow wFromBxs hFromBxs st.inky.pos
+        blinky_pos = Utl.itow wFromBxs hFromBxs st.blinky.pos
+        clyde_pos  = Utl.itow wFromBxs hFromBxs st.clyde.pos
+        pac_pos    = Utl.itow wFromBxs hFromBxs st.pacman.pos
+        pac_dir    = case st.pacman.dir of
+                       Left  -> Mod.Left
+                       Right -> Mod.Right
+                       Up    -> Mod.Up
+                       Down  -> Mod.Down
     in
       El.color black
             <| Clg.collage w h
-                 [ Clg.toForm <| colBuilder (List.map rowBuilder st.board)
-                 , Clg.move pac_pos <| Mod.pacman pac_dir <| toFloat <| bSide // 2
-                 , Clg.move pinky_pos <| renderGhost st.pinky bSide
-                 , Clg.move inky_pos <| renderGhost st.inky bSide
+                 [ Clg.toForm          <| colBuilder (List.map rowBuilder st.board)
+                 , Clg.move pac_pos    <| Mod.pacman pac_dir <| toFloat <| bSide // 2
+                 , Clg.move pinky_pos  <| renderGhost st.pinky bSide
+                 , Clg.move inky_pos   <| renderGhost st.inky bSide
                  , Clg.move blinky_pos <| renderGhost st.blinky bSide
-                 , Clg.move clyde_pos <| renderGhost st.clyde bSide
+                 , Clg.move clyde_pos  <| renderGhost st.clyde bSide
                  , Clg.move gState_pos <| Clg.toForm gState
+                 , if st.gameState == Start then startMenu (min w 350) (min h 400) st else Clg.toForm El.empty
                  ]
 
 --Controller
 
-type Action = KeyAction Key.KeyCode | TimeAction
+type ButtonPressed = Go
+type Action = KeyAction Key.KeyCode | TimeAction | ButtonAction ButtonPressed
+
+actionChannel : Signal.Channel Action
+actionChannel =
+  Signal.channel <| ButtonAction Go
 
 actions : Signal Action
 actions =
-  Signal.merge
-    (Signal.map (\k -> KeyAction k) Key.lastPressed)
-    (Signal.sampleOn (every <| second / 40)  <| Signal.constant TimeAction)
+  Signal.mergeMany
+    [ (Signal.map (\k -> KeyAction k) Key.lastPressed)
+    , (Signal.sampleOn (fps 40)  <| Signal.constant TimeAction)
+    , (Signal.subscribe actionChannel)
+    ]
+    
+    
 
 currState : Signal State
 currState =
   Signal.dropRepeats
+    <| Signal.map (\s -> {s | pellsAte    <- 0,
+                              timer       <- 0,
+                              startTimer  <- 0,
+                              fleeTimer   <- 0,
+                              fleeTimerOn <- False,
+                              ghostPoints <- [],
+                              modeChanges <- [],
+                              defaultMode <- Chase})
     <| Signal.foldp upstate initState actions
 
 allNormal : State -> Bool
@@ -172,17 +241,20 @@ upstate a s =
         in
           Itr.interact
                  <| GCtr.updateGhosts
-                        {s | pacman <- Ctr.updatePacPos s.pacman
-                        , board <- newBoard
-                        , points <- old_pts + extra_pts
-                        , pellsAte <- if atePell then old_pellsAte + 1 else old_pellsAte
-                        , timer <- if s.fleeTimer > 0 then s.timer else s.timer + 0.025
-                        , fleeTimer <- if | stopFlee || not s.fleeTimerOn || atePill -> 0
-                                    | otherwise -> s.fleeTimer + 0.025
-                        , fleeTimerOn <- if | stopFlee  -> False
-                                      | atePill   -> True
-                                      | otherwise -> s.fleeTimerOn
-                        , ghostPoints <- if atePill then  ghostPoints else s.ghostPoints} atePill
+                        {s | pacman      <- Ctr.updatePacPos s.pacman
+                           , board       <- newBoard
+                           , points      <- old_pts + extra_pts
+                           , pellsAte    <- old_pellsAte + (if atePell then 1 else 0)
+                           , timer       <- s.timer + (if s.fleeTimer > 0 then 0 else 0.025)
+                           , fleeTimer   <- if | stopFlee 
+                                                   || not s.fleeTimerOn 
+                                                   || atePill -> 0
+                                               | otherwise    -> s.fleeTimer + 0.025
+                           , fleeTimerOn <- if | stopFlee  -> False
+                                               | atePill   -> True
+                                               | otherwise -> s.fleeTimerOn
+                           , ghostPoints <- if atePill then ghostPoints else s.ghostPoints} atePill
+    (ButtonAction Go, Start) -> {s | gameState <- Loading}
     _ -> s
 -- if extra_pts == 50 -> Pill, then update the ghosts.
 
